f8711a85d1dfbc37984fb69442240b786d329907
Salih Cantekin <salihcantekin@gmail.com>
2022-06-15 23:22:24 +0100

    Add project files.

---
+++ CSharp11Net7.RawStringLiteral/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string firstName = "salih";
+string lastName = "cantekin";
+
+//string name = string.Format("{0}{1}{2}", firstName, " ", lastName);
+// Özel karakter ", {}, $, @, \
+string name = $"{firstName} {lastName}";
+
+name = "{\"name \":\"John \", \"age \":30, \"car \":null}";
+
+name = $$"""""
+        {
+            "name":"{{firstName}}",
+            "age":30, 
+            "car":" """"  "
+        }
+    """"";
+
+Console.WriteLine(name);
+
